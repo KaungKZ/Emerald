2f35d86c104a24e9781fb2bdba943bffbc929df6
Kaung Khant Zaw <kaungkzdev@gmail.com>
2021-04-16 21:42:35 +0630

    changed to table styles for cart items

--- src/components/Layouts/IsEmptyCartPage.jsx
+++ src/components/Layouts/IsEmptyCartPage.jsx
@@ -6,11 +6,13 @@ import alarmClockLine from "@iconify/icons-clarity/alarm-clock-line";
 import CartDetails from "../cart-pages/CartDetails";
 import WishlistDetails from "../cart-pages/WishlistDetails";
 
-const EmptyPageStyles = styled.div`
+const PageStyles = styled.div`
   width: 100%;
   /* margin: 0 auto; */
-  margin: ${props => (props.showCartDetail ? "50px auto" : "0 auto")};
-  min-height: ${props => (props.showCartDetail ? "initial" : "70vh")};
+  margin: ${props =>
+    props.showCartDetail || props.showWishlistDetail ? "50px auto" : "0 auto"};
+  min-height: ${props =>
+    props.showCartDetail || props.showWishlistDetail ? "initial" : "70vh"};
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -18,10 +20,13 @@ const EmptyPageStyles = styled.div`
   justify-content: space-around;
   font-family: var(--small-title-font);
   color: var(--text-color);
+
+  .empty-cart-icon {
+    transform: rotate(-20deg);
+  }
   /* min-height: 70vh; */
 
   svg {
-    transform: rotate(-20deg);
   }
 
   @media (max-width: 768px) {
@@ -29,7 +34,7 @@ const EmptyPageStyles = styled.div`
   }
 `;
 
-const EmptyPageTitle = styled.div`
+const PageTitle = styled.div`
   width: auto;
   margin: ${props => (props.showCartDetail ? "0 auto 65px auto" : "0 auto")};
   /* margin: 0 auto; */
@@ -70,7 +75,7 @@ const EmptyPageTitle = styled.div`
   }
 `;
 
-const EmptySubtitle = styled.h3`
+const Subtitle = styled.h3`
   font-size: 1.2rem;
   font-weight: 400;
   text-align: center;
@@ -88,27 +93,46 @@ const EmptySubtitle = styled.h3`
   }
 `;
 
+const SubtitleWrapper = styled.div``;
+
 export default function IsEmptyCartPage({ children }) {
   // console.log(props);
   // const [isActive, setIsActive] = useState(false);
   const [showCartDetail, setShowCartDetail] = useState(null);
-  const [showWishlistDetail, setShowWishlistDetail] = useState(false);
+  const [showWishlistDetail, setShowWishlistDetail] = useState(null);
   const [selectedProducts, setSelectedProducts] = useState();
 
+  console.log(showCartDetail, showWishlistDetail);
+
   useEffect(() => {
-    if (localStorage.getItem("selectedProduct")) {
-      setShowCartDetail(true);
-      setSelectedProducts(localStorage.getItem("selectedProduct"));
+    if (children === "Shopping cart") {
+      if (localStorage.getItem("selectedProduct")) {
+        setShowCartDetail(true);
+        setSelectedProducts(localStorage.getItem("selectedProduct"));
+      } else {
+        setShowCartDetail(false);
+      }
     } else {
-      setShowCartDetail(false);
+      if (localStorage.getItem("wishlistProducts")) {
+        setShowWishlistDetail(true);
+        setSelectedProducts(localStorage.getItem("wishlistProducts"));
+      } else {
+        setShowWishlistDetail(false);
+      }
     }
   }, []);
 
   return (
-    <EmptyPageStyles showCartDetail={showCartDetail ? true : false}>
-      <EmptyPageTitle showCartDetail={showCartDetail ? true : false}>
+    <PageStyles
+      showCartDetail={showCartDetail ? true : false}
+      showWishlistDetail={showWishlistDetail ? true : false}
+    >
+      <PageTitle
+        showCartDetail={showCartDetail ? true : false}
+        showWishlistDetail={showWishlistDetail ? true : false}
+      >
         <h1 className="title">{children}</h1>
-      </EmptyPageTitle>
+      </PageTitle>
       {children === "Shopping cart" ? (
         !showCartDetail ? (
           <>
@@ -117,7 +141,9 @@ export default function IsEmptyCartPage({ children }) {
               style={{ color: "#606060", fontSize: "110.85014343261719px" }}
               className="empty-cart-icon"
             />
-            <EmptySubtitle>There are no items in your cart</EmptySubtitle>
+            <SubtitleWrapper>
+              <Subtitle>There are no items in your cart</Subtitle>
+            </SubtitleWrapper>
           </>
         ) : (
           <CartDetails selectedProducts={selectedProducts}></CartDetails>
@@ -129,11 +155,13 @@ export default function IsEmptyCartPage({ children }) {
             style={{ color: "#606060", fontSize: "110.85014343261719px" }}
             className="empty-cart-icon"
           />
-          <EmptySubtitle>There are no items in your wishlist</EmptySubtitle>
+          <SubtitleWrapper>
+            <Subtitle>There are no items in your wishlist</Subtitle>
+          </SubtitleWrapper>
         </>
       ) : (
         <WishlistDetails></WishlistDetails>
       )}
-    </EmptyPageStyles>
+    </PageStyles>
   );
 }
--- src/components/cart-pages/CartDetails.jsx
+++ src/components/cart-pages/CartDetails.jsx
@@ -1,8 +1,52 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import Img from "gatsby-image";
+import { Icon, InlineIcon } from "@iconify/react";
+import crossMarkButton from "@iconify/icons-emojione-monotone/cross-mark-button";
+// import { Icon, InlineIcon } from '@iconify/react';
+import femaleIcon from "@iconify/icons-fa-solid/female";
+import maleIcon from "@iconify/icons-fa-solid/male";
+// import IconButton from "@material-ui/core/IconButton";
 
 // import cart from "../../pages/cart";
+
+const Table = styled.div`
+  width: 100%;
+  display: table;
+
+  #resp-table-header {
+    display: table-header-group;
+    background-color: gray;
+    font-weight: bold;
+    font-size: 25px;
+
+    .table-header-cell {
+      display: table-cell;
+      padding: 10px;
+      text-align: justify;
+      border-bottom: 1px solid black;
+    }
+  }
+
+  #resp-table-body {
+    display: table-row-group;
+
+    .resp-table-row {
+      display: table-row;
+
+      .table-body-cell {
+        display: table-cell;
+      }
+    }
+  }
+`;
+
+const CartNavigators = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 90%;
+  margin: 0 auto;
+`;
 
 const CartWrapper = styled.div`
   width: 100%;
@@ -13,24 +57,59 @@ const CartItems = styled.div`
   flex-direction: column;
 `;
 
+const CartItemWrapper = styled.div`
+  width: 100%;
+  background: #f5f5f5;
+  border-bottom: 1px solid rgba(96, 96, 96, 0.1);
+  min-height: 150px;
+  height: 1px;
+  &:not(:last-child) {
+    margin-bottom: 15px;
+  }
+
+  &:first-child {
+    border-top: 1px solid rgba(96, 96, 96, 0.1);
+  }
+`;
+
 const CartItem = styled.div`
+  width: 90%;
+  height: 100%;
+  margin: 0 auto;
   display: flex;
   justify-content: space-between;
+  align-items: center;
 `;
 
 const CancelIcon = styled.div``;
+
+const IconWrapper = styled.button`
+  outline: none;
+  border: none;
+  background: none;
+  cursor: pointer;
+  /* svg {
+    transform: rotate(0deg) !important;
+  } */
+`;
 
 const ItemDetailsWrapper = styled.div`
   display: flex;
   .item-image {
     .gatsby-image-wrapper {
-      /* width: 145px !important;
-        height: 145px !important;
-        max-height: 145px !important; */
-
       picture img {
         object-fit: contain !important;
       }
+    }
+  }
+
+  .item-details {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    & > div:not(:last-child) {
+      margin-bottom: 7px;
     }
   }
 `;
@@ -50,6 +129,7 @@ export default function CartDetails({ selectedProducts }) {
         "price",
         "productQty",
         "size",
+        "gender",
         "images",
       ]) {
         if (cur.hasOwnProperty(value)) {
@@ -67,36 +147,127 @@ export default function CartDetails({ selectedProducts }) {
   // console.log(cartItems);
   return (
     <>
-      <CartWrapper>
-        <CartItems>
+      <Table id="res-table">
+        <div id="resp-table-header">
+          <div class="table-header-cell">Items</div>
+          <div class="table-header-cell">Items</div>
+          <div class="table-header-cell">Items</div>
+          <div class="table-header-cell">Items</div>
+        </div>
+        <div id="resp-table-body">
           {cartItems.map(item => {
             return (
-              <CartItem>
-                <ItemDetailsWrapper>
-                  <div className="item-image">
-                    <Img
-                      fixed={item.images[0].fixed}
-                      style={{ maxHeight: "100px" }}
-                      className="product-image"
-                    />
+              <div class="resp-table-row">
+                <div class="table-body-cell">
+                  <div className="item-detail-wrapper">
+                    <div className="item-image">
+                      <Img
+                        fixed={item.images[0].fixed}
+                        style={{ maxHeight: "90px" }}
+                        className="product-image"
+                      />
+                    </div>
+                    <div className="item-details">
+                      <div className="item-name">{item.title}</div>
+                      <div className="item-size">{item.size}</div>
+                      {item.gender && (
+                        <div className="item-gender">
+                          {item.gender === "female" ? (
+                            <Icon
+                              icon={femaleIcon}
+                              style={{ color: "#5a5a5a", fontSize: "21px" }}
+                            />
+                          ) : (
+                            <Icon
+                              icon={maleIcon}
+                              style={{ color: "#5a5a5a", fontSize: "21px" }}
+                            />
+                          )}
+                        </div>
+                      )}
+                    </div>
                   </div>
-                  <div className="item-details">
-                    <div className="item-name">{item.title}</div>
-                    <div className="item-size">{item.size}</div>
-                  </div>
-                </ItemDetailsWrapper>
-                <ItemQuantity>
-                  <div className="quantity">{item.productQty}</div>
-                </ItemQuantity>
-                <ItemAmount>
-                  <div className="amount">{item.price}</div>
-                </ItemAmount>
-                <CancelIcon></CancelIcon>
-              </CartItem>
+                </div>
+                <div class="table-body-cell">{item.productQty}</div>
+                <div className="table-body-cell">{item.price}</div>
+                <div className="table-body-cell">
+                  <CancelIcon>
+                    <IconWrapper>
+                      <Icon
+                        icon={crossMarkButton}
+                        style={{ color: "#606060", fontSize: "25px" }}
+                      />
+                    </IconWrapper>
+                  </CancelIcon>
+                </div>
+              </div>
             );
           })}
-        </CartItems>
-      </CartWrapper>
+        </div>
+        {/* <CartWrapper> */}
+        {/* <CartNavigators>
+            <span>Items</span>
+            <span>Qty</span>
+            <span>Amount</span>
+            <span>Amount</span>
+          </CartNavigators> */}
+
+        {/* <CartItems>
+            {cartItems.map(item => {
+              return (
+                <CartItemWrapper>
+                  <CartItem>
+                    <ItemDetailsWrapper>
+                      <div className="item-image">
+                        <Img
+                          fixed={item.images[0].fixed}
+                          style={{ maxHeight: "90px" }}
+                          className="product-image"
+                        />
+                      </div>
+                      <div className="item-details">
+                        <div className="item-name">{item.title}</div>
+                        <div className="item-size">{item.size}</div>
+                        {item.gender && (
+                          <div className="item-gender">
+                            {item.gender === "female" ? (
+                              <Icon
+                                icon={femaleIcon}
+                                style={{ color: "#5a5a5a", fontSize: "21px" }}
+                              />
+                            ) : (
+                              <Icon
+                                icon={maleIcon}
+                                style={{ color: "#5a5a5a", fontSize: "21px" }}
+                              />
+                            )}
+                          </div>
+                        )}
+                      </div>
+                    </ItemDetailsWrapper>
+                    <ItemQuantity>
+                      <div className="quantity">{item.productQty}</div>
+                    </ItemQuantity>
+                    <ItemAmount>
+                      <div className="amount">{item.price}</div>
+                    </ItemAmount>
+                    <CancelIcon>
+
+                      <IconWrapper>
+                        <Icon
+                          icon={crossMarkButton}
+                          style={{ color: "#606060", fontSize: "25px" }}
+                        />
+                      </IconWrapper>
+
+                    </CancelIcon>
+                  </CartItem>
+                </CartItemWrapper>
+              );
+            })}
+          </CartItems> */}
+        {/* </CartWrapper> */}
+      </Table>
     </>
   );
 }
